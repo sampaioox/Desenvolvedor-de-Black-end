db8868cd0a12efa117e7ea21e7ac9e8bf21827ef
Anna sampaio <sampaioannah1@gmail.com>
2025-09-10 21:50:59 -0300

    Add files via upload

---
+++ ana.py 1.py
@@ -0,0 +1,79 @@
+#calcule a soma de 15 + 27
+a = 15
+b = 27
+print(a + b)
+
+#multiplique 12 por 8
+a = 12
+b = 8
+print(a * b)
+
+#dividida 25 por 4 e veja o reseltado
+a = 25
+b = 4
+print(a / b)
+
+#descubra o resto da divisão de 37 por 5
+a = 37
+b = 5
+print(a % b)
+
+#eleve 3 á potencia de 4
+a = 3
+b = 4
+print(a ** b)
+
+
+#verifique se o 10 é maior que 7
+a = 10
+b = 7
+print(a > b)
+
+#verrifique se 15 é igual a 20
+a = 15
+b = 20
+print(a == b)
+
+#teste se 100 é menor ou igual a 100
+a = 100
+b = 100
+print(a <= b)
+
+#veja se 45 é diferente de 30
+a = 45
+b = 30
+print(a != b)
+
+
+#verifique se 5 > 2 e 8 > 3
+a = 5
+b = 2
+c = 8
+d = 3
+print(a > b and c > d)
+
+#verifique se 10 < 3 ou 4 == 4
+a = 10
+b = 3
+c = 4
+d = 4
+print(a < b or c == d)
+
+#inverta o resultado lógico de (7 > 2) usando not
+a = 7
+b = 2 
+print(not (a > b))
+
+
+#crie uma variável x com o valor 10
+x = 10
+
+x += 5
+print(10 + 5)
+x *= 2 
+print(10 * 2)
+x -= 4
+print(10 - 4)
+#mostre o valor final de x
+
+
